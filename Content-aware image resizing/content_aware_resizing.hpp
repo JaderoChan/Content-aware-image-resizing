d8cd2b23c858365b30398aacc1a65e76159251a2
頔珞 <c_dl_cn@outlook.com>
2025-03-25 18:38:45 +0800

    Delete Content-aware image resizing directory

--- Content-aware image resizing/content_aware_resizing.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-#include <opencv2/opencv.hpp>
-
-cv::Mat makeEnergyImage(const cv::Mat& img);
-
-cv::Mat removeMinimumEnergyCloumn(const cv::Mat& img);
--- Content-aware image resizing/temp_file_path.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <qstring.h>
-
-QString tempFilePath1();
-
-QString tempFilePath2();
