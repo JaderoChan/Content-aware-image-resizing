4c051f9c90db87ec0a97decf72772b9398573137
頔珞 <c_dl_cn@outlook.com>
2025-04-11 17:49:35 +0800

    Update image_process.hpp
    更改函数名lightnessEnhance为enhanceLightness

--- Content-aware image resizing/Content-aware image resizing/image_process.hpp
+++ Content-aware image resizing/Content-aware image resizing/image_process.hpp
@@ -4,7 +4,7 @@
 
 /// @brief 增强图像亮度。
 /// @param multiple 用于乘以图像矩阵的倍数。
-cv::Mat lightnessEnhance(const cv::Mat& img, double multiple);
+cv::Mat enhanceLightness(const cv::Mat& img, double multiple);
 
 /// @brief 将图像缩放在指定大小内，且保留长宽比。
 cv::Mat limitSize(const cv::Mat& img, int maxWidth, int maxHeight);
