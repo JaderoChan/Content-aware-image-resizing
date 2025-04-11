db4d15ae94b6f2c67076331b67cf1ee374091d98
頔珞 <c_dl_cn@outlook.com>
2025-04-11 17:47:05 +0800

    Update image_process.hpp
    - 删除不必要注释
    - 更改函数名limitedSize为limitSize

--- Content-aware image resizing/Content-aware image resizing/image_process.hpp
+++ Content-aware image resizing/Content-aware image resizing/image_process.hpp
@@ -3,12 +3,8 @@
 #include <opencv2/opencv.hpp>
 
 /// @brief 增强图像亮度。
-/// @param img 图像。
 /// @param multiple 用于乘以图像矩阵的倍数。
 cv::Mat lightnessEnhance(const cv::Mat& img, double multiple);
 
 /// @brief 将图像缩放在指定大小内，且保留长宽比。
-/// @param img 图像。
-/// @param maxWidth 最大宽度。
-/// @param maxHeight 最大高度。
 cv::Mat limitedSize(const cv::Mat& img, int maxWidth, int maxHeight);
